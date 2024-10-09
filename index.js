d73c3b3ec1edc7f4239c77a40f0b2e331ff41c3e
Brent Wise <brentjosephwise@gmail.com>
2024-10-09 10:00:06 -0700

    Create API for whoami, Update README, ADD vercel.json, Update Package.json

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 // index.js
-// where your node app starts
 
-// init project
+// Import required modules
 require('dotenv').config();
 var express = require('express');
 var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+// Enable CORS to allow testing from external sources
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// Basic route to serve the homepage
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
+// Simple hello endpoint
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// listen for requests :)
+// /api/whoami endpoint to return user IP address, language, and software information
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  });
+});
+
+// Listen for requests
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
